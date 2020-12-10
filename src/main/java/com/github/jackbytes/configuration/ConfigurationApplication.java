f27838a8f2406ba64813caf86bf1b8d70859486e
Jackwang <78700393@qq.com>
2020-12-10 14:26:20 +0800

    init

---
+++ src/main/java/com/github/jackbytes/configuration/ConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.github.jackbytes.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationApplication.class, args);
+    }
+
+}
